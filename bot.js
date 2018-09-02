1195d9cd9652b05920b7dbc8b0913825404969d9
willikesgames <blogboywill@gmail.com>
2018-09-02 14:17:40 -0400

    Added a responce if someone types any form of owo

--- bot.js
+++ bot.js
@@ -12,9 +12,10 @@ client.on('message', async (message) => {
         if(message.author.bot) return;
         const args = message.content.trim().split(/ +/g);
         if((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im') && (args[1]) && !client.disabledMembers.has(message.author.id)){
-                client.user.setPresence({ status: 'online', game: { name: `with my ${client.users.array().length} kids` } });
                 message.channel.send(`Hi ${args.slice(1).join(' ')}, I'm dad`);
         }
+        owos = ['owo', 'ovo', 'uwu', 'uvu', 'unu', 'ono', 'umu', 'ene', 'nun'];
+        if(owos.includes(args[0].toLowerCase) && !client.disabledMembers.has(message.author.id)) message.channel.send('<:object:451062455762681866>');
         if(message.isMemberMentioned(client.user)){
                 client.user.setPresence({ status: 'online', game: { name: `with my ${client.users.array().length} kids` } });
                 if(message.content.indexOf('stop') !== -1) client.disabledMembers.set(message.author.id, message.guild.id)
